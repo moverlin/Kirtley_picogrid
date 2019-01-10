01e29d8baa6449e1c43766d90fed855c2e21845d
Colm <colmjorourke@gmail.com>
2019-01-10 16:11:47 -0500

    adding Mike's 3-inverter DEMO.  Important!  Don't touch this!! Mike is leaving, keep it as a reference project.

--- Software/3_INV_DEMO/Inveter_RS485/CPU2/CPU2_FLASH/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/git/MIT-Pico-Grid/Project/New_MG/Firmware/Inveter_RS485/CPU2/F2837xD_common/cmd/2837xD_FLASH_lnk_cpu2.cmd" -l"C:/git/MIT-Pico-Grid/Project/New_MG/Firmware/Inveter_RS485/CPU2/F2837xD_headers/cmd/F2837xD_Headers_nonBIOS_cpu2.cmd" -lrts2800_fpu32.lib -llibc.a
-
